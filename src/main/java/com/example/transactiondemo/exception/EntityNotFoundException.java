de1311875f73e5b3a4f8298600a576eba25aff6d
Ahmed Nafisul Bari <ahmed.bari@midasteknologi.com>
2022-07-12 18:01:53 +0600

    Added exception class

---
+++ src/main/java/com/example/transactiondemo/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.transactiondemo.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}

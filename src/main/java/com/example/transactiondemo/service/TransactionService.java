c2bd953a11527021df32147cc2d8a21046c0d168
Ahmed Nafisul Bari <ahmed.bari@midasteknologi.com>
2022-07-12 18:21:29 +0600

    Implemented basic logging with Log4j2 using lombok annotations

--- src/main/java/com/example/transactiondemo/service/TransactionService.java
+++ src/main/java/com/example/transactiondemo/service/TransactionService.java
@@ -14,6 +14,7 @@ import com.example.transactiondemo.exception.EntityNotFoundException;
 import com.example.transactiondemo.repository.AccountDetailsRepository;
 import com.example.transactiondemo.repository.TransactionDetailsRepository;
 import com.example.transactiondemo.util.RandomUtil;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +23,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Log4j2
 public class TransactionService {
 
     @Autowired
@@ -31,13 +33,17 @@ public class TransactionService {
     private TransactionDetailsRepository transactionDetailsRepository;
 
     public FundTransferResponse fundTransfer(FundTransferRequest request) {
-        AccountDetails debitAccount = getAccountDetailsByAccountNo(request.getToAccount());
+        AccountDetails debitAccount = getAccountDetailsByAccountNo(request.getFromAccount());
         debitAccount.setAvailableBalance(debitAccount.getAvailableBalance() - request.getAmount());
         accountDetailsRepository.save(debitAccount);
+        log.info("Debited: " + request.getAmount() +
+                " from account: " + request.getFromAccount());
 
         AccountDetails creditAccount = getAccountDetailsByAccountNo(request.getToAccount());
         creditAccount.setAvailableBalance(creditAccount.getAvailableBalance() + request.getAmount());
         accountDetailsRepository.save(creditAccount);
+        log.info("Credited: " + request.getAmount() +
+                " from account: " + request.getToAccount());
 
         int debitReferenceNumber = processTransaction(request);
 
@@ -62,6 +68,8 @@ public class TransactionService {
         debitTransaction.setTransactionAmount(request.getAmount());
 
         transactionDetailsRepository.save(debitTransaction);
+        log.info("Saving transaction record: " + debitTransactionId.getTransactionFlag() +
+                " from account: " + debitTransactionId.getAccountNo());
 
         TransactionDetailsId creditTransactionId = new TransactionDetailsId();
         creditTransactionId.setTransactionFlag("C");
@@ -73,6 +81,8 @@ public class TransactionService {
         creditTransaction.setTransactionAmount(request.getAmount());
 
         transactionDetailsRepository.save(creditTransaction);
+        log.info("Saving transaction record: " + creditTransactionId.getTransactionFlag() +
+                " from account: " + creditTransactionId.getAccountNo());
 
         return debitTransactionId.getReferenceNumber();
     }
@@ -81,6 +91,8 @@ public class TransactionService {
 
         AccountDetails accountDetails = getAccountDetailsByAccountNo(request.getAccountNo());
         double balance = accountDetails.getAvailableBalance();
+        log.info("Found balance of account: " + accountDetails.getAccountNo() +
+                " balance: " + accountDetails.getAvailableBalance());
 
         return new BalanceResponse(balance);
     }
@@ -102,6 +114,7 @@ public class TransactionService {
     private AccountDetails getAccountDetailsByAccountNo(int accountNo) {
         Optional<AccountDetails> optionalAccountDetails = accountDetailsRepository.findByAccountNo(accountNo);
         if (!optionalAccountDetails.isPresent()) {
+            log.info("Account not found in db: " + accountNo);
             throw new EntityNotFoundException("Account details not found");
         }
 
--- src/main/java/com/example/transactiondemo/service/UserService.java
+++ src/main/java/com/example/transactiondemo/service/UserService.java
@@ -5,12 +5,14 @@ import com.example.transactiondemo.entity.User;
 import com.example.transactiondemo.entity.UserId;
 import com.example.transactiondemo.exception.DuplicateEntityException;
 import com.example.transactiondemo.repository.UserRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@Log4j2
 public class UserService {
 
     @Autowired
@@ -20,6 +22,7 @@ public class UserService {
 
         Optional<User> optionalUser = userRepository.findByUserIdUserId(request.getUserId());
         if (optionalUser.isPresent()) {
+            log.info("Duplicate user creation, user already exists with id: " + request.getUserId());
             throw new DuplicateEntityException("user already exists");
         }
 
